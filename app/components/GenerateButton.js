4b15518ced7a1249f57db6d6a5384088fee4fb65
Han-Shen Chen <digenes@hotmail.de>
2023-10-11 18:17:05 +0200

    v2

---
+++ app/components/GenerateButton.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function GenerateButton() {
+  return (
+    <div>
+      <button>Generate</button>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
-      <h1>HI</h1>
+      <h1>Hi</h1>
     </main>
-  )
+  );
 }
